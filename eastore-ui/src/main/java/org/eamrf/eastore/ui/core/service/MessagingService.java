50455f78f82597fd81805676fa250b3b826426b5
slenzi <lenzi@jimmy.harvard.edu>
2018-04-20 15:33:42 -0400

    added retry logic when establishing websocket connection to eastore

--- eastore-ui/src/main/java/org/eamrf/eastore/ui/core/service/MessagingService.java
+++ eastore-ui/src/main/java/org/eamrf/eastore/ui/core/service/MessagingService.java
@@ -2,6 +2,11 @@ package org.eamrf.eastore.ui.core.service;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -9,14 +14,23 @@ import javax.annotation.PreDestroy;
 import org.eamrf.core.exception.ServiceException;
 import org.eamrf.core.logging.stereotype.InjectLogger;
 import org.eamrf.eastore.ui.core.properties.ManagedProperties;
-import org.eamrf.eastore.ui.core.socket.messaging.client.GenericSessionHandler;
+import org.eamrf.eastore.ui.core.socket.messaging.client.GenericStompSessionHandler;
 import org.eamrf.eastore.ui.core.socket.messaging.client.StompWebSocketService;
 import org.eamrf.eastore.ui.core.socket.messaging.model.ResourceChangeMessage;
 import org.eamrf.eastore.ui.core.socket.messaging.server.ResourceChangeBroadcastService;
 import org.slf4j.Logger;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.stereotype.Service;
+
+import com.github.rholder.retry.RetryException;
+import com.github.rholder.retry.Retryer;
+import com.github.rholder.retry.RetryerBuilder;
+import com.github.rholder.retry.StopStrategies;
+import com.github.rholder.retry.WaitStrategies;
+import com.google.common.base.Predicates;
 
 /**
  * Handles sending and receiving messages over websockets.
@@ -38,47 +52,85 @@ public class MessagingService {
     @Autowired
     private ResourceChangeBroadcastService resourceChangeBroadcaster;
     
+    private final Marker websocketMarker = MarkerFactory.getMarker("[WebSocket]");
+    
+    private ExecutorService executorService = Executors.newFixedThreadPool(1);
+    
 	// Subscription to EA Store 'resource change' messages
-	//
 	// @see eastore codebase:
 	// org.eamrf.eastore.core.socket.messaging.ResourceChangeService
 	// org.eamrf.eastore.core.config.WebSocketConfig
 	private final String EA_STORE_RESOURCE_CHANGE_STOMP_SUBSCRIPTION = "/topic/resource/change";
 	
-	// handler which receives incoming resource change events from eastore, then rebroadcasts them
-	// back out over the eastore-ui stomp websocket
-	private GenericSessionHandler<ResourceChangeMessage> resourceChangeSessionHandler = new GenericSessionHandler<ResourceChangeMessage>(
+	// handler which receives incoming resource change events from eastore, then re-broadcasts them
+	// over the eastore-ui stomp websocket
+	private GenericStompSessionHandler<ResourceChangeMessage> resourceChangeSessionHandler = new GenericStompSessionHandler<ResourceChangeMessage>(
 			EA_STORE_RESOURCE_CHANGE_STOMP_SUBSCRIPTION,
+			ResourceChangeMessage.class,
 			message -> {
-				
-				logger.info("Received resource change message, " + message.toString());
-				
+				logger.info(websocketMarker, "Received resource change message from eastore, " + message.toString());
 				resourceChangeBroadcaster.broadcast(message);
-				
-			}, ResourceChangeMessage.class);	
+			},
+			(stompSession, throwable) -> {
+				logger.info(websocketMarker, "Session {} was lost, {}", stompSession.getSessionId(), throwable.getMessage(), throwable);
+				logger.info("Attempting to re-establish stomp websocket connection to eastore.");
+				initStompConnection();
+			});	
 	
 	public MessagingService() { }
 	
+	/**
+	 * Initialize messaging service by connecting to eastore stomp websocket endpoint and
+	 * subscribing to the "resource change" destination. If a connection to eastore cannot
+	 * be made then it will continue to retry until successful. Once a connection has been
+	 * made, all incoming resource change messages will be re-broadcasted via the eastore-ui
+	 * websocket endpoint. 
+	 */
 	@PostConstruct
-	private void init() {
+	private void initStompConnection() {
 		
-		logger.info("Initializing stomp websocket connections...");
+		logger.info(websocketMarker, "Initializing eastore stomp websocket connection...");
 		
-		// TODO
+		Callable<StompSession> connectCallable = new Callable<StompSession>() {
+		    public StompSession call() throws Exception {
+		    	StompSession stompSession = null;
+		    	try {
+		    		stompSession = stompSocketService.connect(getEaStoreEndpoint(), resourceChangeSessionHandler);
+				} catch (ServiceException e) {
+					logger.error(websocketMarker, "Failed to connect to eastore and subscribe to resource "
+							+ "change events, " + e.getMessage());
+					return null;
+				}
+		    	return stompSession;
+		    }
+		};
 		
-		// How to handle undesired disconnections? Do we need to create a thread
-		// which periodically checks the state of the session and reconnect if
-		// it's closed? e.g., eastore service is shutdown/re-deployed.
+		Retryer<StompSession> retryer = RetryerBuilder.<StompSession>newBuilder()
+				.retryIfResult(Predicates.<StompSession>isNull())
+		        .retryIfResult(stompSession -> {
+		        	if(!stompSession.isConnected()) {
+		        		logger.info(websocketMarker, "Stomp session for eastore resource change events is not connected. Retrying again...");
+		        		return true;
+		        	}
+		        	return false;
+		        })
+		        .withWaitStrategy(WaitStrategies.fixedWait(30, TimeUnit.SECONDS))
+		        .withStopStrategy(StopStrategies.neverStop())
+		        .build();
 		
-		StompSession stompSession = null;
-		try {
-			stompSession = stompSocketService.connect(getEaStoreEndpoint(), resourceChangeSessionHandler);
-		} catch (ServiceException e) {
-			logger.error("Websocket error, failed to listen for eastore resource change events. " + e.getMessage());
-			e.printStackTrace();
-		}
-		
-		logger.info("Initialization of stomp websocket connections complete!");
+		executorService.execute(new Runnable() {
+		    public void run() {
+				StompSession stompSession = null;
+				try {
+					stompSession = retryer.call(connectCallable);
+				} catch (RetryException e) {
+				    e.printStackTrace();
+				} catch (ExecutionException e) {
+				    e.printStackTrace();
+				}		    	
+				logger.info(websocketMarker, "Initialization of stomp websocket connections complete!");
+		    }
+		});
 		
 	}
 	
@@ -98,11 +150,11 @@ public class MessagingService {
 	@PreDestroy
 	private void destroy() {
 		
-		logger.info("Closing stomp websocket connections...");
+		logger.info(websocketMarker, "Closing stomp websocket connections...");
 		
 		stompSocketService.closeAllSessions(true);
 		
-		logger.info("Closing of stomp websocket connections complete!");
+		logger.info(websocketMarker, "Closing of stomp websocket connections complete!");
 		
 	}
 
--- eastore-ui/src/main/java/org/eamrf/eastore/ui/core/socket/messaging/client/GenericStompSessionHandler.java
+++ eastore-ui/src/main/java/org/eamrf/eastore/ui/core/socket/messaging/client/GenericStompSessionHandler.java
@@ -4,10 +4,12 @@
 package org.eamrf.eastore.ui.core.socket.messaging.client;
 
 import java.lang.reflect.Type;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.stomp.ConnectionLostException;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
@@ -18,25 +20,30 @@ import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
  * 
  * @author slenzi
  */
-public class GenericSessionHandler<T> extends StompSessionHandlerAdapter {
+public class GenericStompSessionHandler<T> extends StompSessionHandlerAdapter {
 
-	private Logger logger = LoggerFactory.getLogger(GenericSessionHandler.class.getName());
+	private Logger logger = LoggerFactory.getLogger(GenericStompSessionHandler.class.getName());
     
 	private String destination = null;
-    private Consumer<T> handleFrameConsumer = null;
-    private Class<T> typeParameterClass = null;
+	private Class<T> typeParameterClass = null;
+    private Consumer<T> frameConsumer = null;
+    private BiConsumer<StompSession, Throwable> disconnectConsumer = null;
 	
 	/**
 	 * Create instance of resource change session handler
 	 * 
-	 * @param destination - the destination to subscribe to 
-	 * @param handleFrameConsumer - the consumer that will process incoming resource change messages
-	 * @param typeParameterClass - the class type of resource change message
+	 * @param destination - the destination to subscribe to
+	 * @param typeParameterClass - the class type of the incoming stomp frame (message)
+	 * @param frameConsumer - the consumer that will process incoming resource change messages
+	 * @param disconnectConsumer - the consumer that handles disconnect events
 	 */
-	public GenericSessionHandler(String destination, Consumer<T> handleFrameConsumer, Class<T> typeParameterClass) {
+	public GenericStompSessionHandler(String destination, Class<T> typeParameterClass, 
+			Consumer<T> frameConsumer, BiConsumer<StompSession, Throwable> disconnectConsumer) {
+		
 		this.destination = destination;
-		this.handleFrameConsumer = handleFrameConsumer;
 		this.typeParameterClass = typeParameterClass;
+		this.frameConsumer = frameConsumer;
+		this.disconnectConsumer = disconnectConsumer;
 	}
 
 	/* (non-Javadoc)
@@ -46,7 +53,7 @@ public class GenericSessionHandler<T> extends StompSessionHandlerAdapter {
 	public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
 
 		logger.info("New session established : " + session.getSessionId());
-		
+
 		session.subscribe(destination, this);
 		
 	}
@@ -75,11 +82,36 @@ public class GenericSessionHandler<T> extends StompSessionHandlerAdapter {
 	/* (non-Javadoc)
 	 * @see org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter#handleFrame(org.springframework.messaging.simp.stomp.StompHeaders, java.lang.Object)
 	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public void handleFrame(StompHeaders headers, Object payload) {
 		
-		handleFrameConsumer.accept((T) payload);
+		if(frameConsumer != null) {
+			frameConsumer.accept((T) payload);
+		}
 		
 	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter#handleTransportError(org.springframework.messaging.simp.stomp.StompSession, java.lang.Throwable)
+	 */
+	@Override
+	public void handleTransportError(StompSession session, Throwable e) {
+		
+		//logger.error(websocketMarker,"Transport error, {}", e.getMessage(), e);
+		
+		if (e instanceof ConnectionLostException) {
+			
+			//logger.error(websocketMarker,"Connection lost, {}", e.getMessage(), e);
+			
+			if(disconnectConsumer != null) {
+				disconnectConsumer.accept(session, e);
+			}
+			
+		}
+	
+	}
+	
+
 
 }
